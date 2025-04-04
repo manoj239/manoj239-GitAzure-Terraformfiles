129aac4b14d5bcddba1ef3a3f2a5eb6e44880fb9
MANOJ <manojborra@ebsco.com>
2025-04-04 18:11:47 +0530

    Updated Backend

---
+++ 4.StorageAccount.tf
@@ -0,0 +1,31 @@
+resource "azurerm_storage_account" "devsecopsmanojtfstate" {
+  name                     = "devsecopsmanojtfstate"
+  resource_group_name      = azurerm_resource_group.Enclave1.name
+  location                 = azurerm_resource_group.Enclave1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Dev"
+  }
+}
+
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.devsecopsmanojtfstate.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "demodata" {
+  name                  = "demodata"
+  storage_account_id    = azurerm_storage_account.devsecopsmanojtfstate.id
+  container_access_type = "private"
+} 
+
+#resource "azurerm_storage_container" "megastar" {
+#  name                  = "megastar"
+#  storage_account_id    = azurerm_storage_account.devsecopsmanojtfstate
+#  container_access_type = "private"
+#} 
+    
