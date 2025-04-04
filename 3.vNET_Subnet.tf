7060673f84d5dd5477bac988cbed643a98bfd1d6
MANOJ <manojborra@ebsco.com>
2025-04-04 17:58:23 +0530

    Added VNET and subnets

---
+++ 3.vNET_Subnet.tf
@@ -0,0 +1,57 @@
+#variable vnet1_address_space {
+#    type       = list(string)
+#    default    = ["10.43.0.0/16"]
+#}
+
+#variable vnet1_subnet1 {
+#    type      = list(string)
+#    default   = [ "10.43.1.0/24" ]
+#}
+#
+#variable vnet1_subnet2 {
+#    type      = list(string)
+#    default   = [ "10.43.2.0/24" ]
+#}
+#
+#variable vnet1_subnet3 {
+#    type      = list(string)
+#    default   = [ "10.43.3.0/24" ]
+#}
+
+
+resource "azurerm_virtual_network" "vnet1" {
+  name = "vnet1"
+  #Implicit Dependency for location & Resource Group on Enclave1
+  location            = azurerm_resource_group.Enclave1.location
+  resource_group_name = azurerm_resource_group.Enclave1.name
+  address_space       = ["10.43.0.0/16"]
+  #address_space      = var.vnet1_address_space
+
+  tags = {
+    environment = "Dev"
+    #environment = var.env
+  }
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "${azurerm_resource_group.Enclave1.name}-subnet-1"
+  resource_group_name  = azurerm_resource_group.Enclave1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.43.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "${azurerm_resource_group.Enclave1.name}-subnet-1"
+  resource_group_name  = azurerm_resource_group.Enclave1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.43.2.0/24"]
+  #vNET_Subnet.tf
+}
+
+resource "azurerm_subnet" "subnet3" {
+  name                 = "${azurerm_resource_group.Enclave1.name}-subnet-1"
+  resource_group_name  = azurerm_resource_group.Enclave1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.43.3.0/24"]
+  #vNET_Subnet.tf
+}
