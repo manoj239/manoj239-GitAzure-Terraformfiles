b66fb9c9182ac5a3d6e5d03ad0c106122276d0e9
MANOJ <manojborra@ebsco.com>
2025-04-06 21:58:08 +0530

    Adding 2 files

---
+++ 6.data_keyvault.tf
@@ -0,0 +1,28 @@
+data "azurerm_key_vault" "Azueusingterraform" {
+  provider            = azurerm.prod-subscription
+  name                = "Azueusingterraform"
+  resource_group_name = "COMMON-RD"
+}
+
+resource "azurerm_key_vault_secret" "password1" {
+    provider     = azurerm.prod-subscription
+    name         = "password-1"
+    value        =  random_password.password1.result
+    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+}
+
+resource "azurerm_key_vault_secret" "password2" {
+    provider     = azurerm.prod-subscription
+    name         = "password-2"
+    value        =  random_password.password2.result
+    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+}
+
+resource "azurerm_key_vault_secret" "password3" {
+    provider     = azurerm.prod-subscription
+    name         = "password-3"
+    value        =  random_password.password3.result
+    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+}
+
+
