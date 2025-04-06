893a3e7f931f7ab43fadc8d3bc7d4f35601d16f4
MANOJ <manojborra@ebsco.com>
2025-04-06 22:42:00 +0530

    Added variables

--- 2.Resourcegroup.tf
+++ 2.Resourcegroup.tf
@@ -1,56 +1,12 @@
 resource "azurerm_resource_group" "Enclave1" {
-  name     = "Enclave1"
-  location = "eastus"
+  name     = var.rg1_name
+  location = var.rg1_location
+  tags = {
+    environment = var.env
+  }
 }
 
-#resource "azurerm_resource_group" "Enclave2" {
-#  name     = "Enclave2"
-#  location = "westus"
-#}
-
-#resource "azurerm_resource_group" "Enclave3" {
-#  name     = "Enclave3"
-#  location = "westus2"
-#}
-
-#--------------------------------
-
-#variable rg1_name {
-#  type        = string
-#  default     = "Enclave1"
-#}
-
-#variable rg2_name {
-#  type        = string
-#  default     = "Enclave2"
-#}
-
-#variable rg1_location {
-#  type        = string
-#  default     = "eastus"
-#}
-
-#variable rg2_location {
-#  type        = string
-#  default     = "westus"
-#}
-
-#----------
-#variable "env" {
-#  type      = string 
-#  default   = "dev"
-#}
-
-
-#resource "azurerm_resource_group" "Enclave1" {
-#  name     =  var.rg1_name
-#  location =  var.rg1_location
-#  tags = {
-#     environment = var.env
-#  }
-#}
-#
-#resource "azurerm_resource_group" "Enclave2" {
-#  name     =  var.rg2_name
-#  location =  var.rg2_location
-#}
+resource "azurerm_resource_group" "Enclave2" {
+  name     = var.rg2_name
+  location = var.rg2_location
+}
--- 6.data_keyvault.tf
+++ 6.data_keyvault.tf
@@ -5,24 +5,24 @@ data "azurerm_key_vault" "Azueusingterraform" {
 }
 
 resource "azurerm_key_vault_secret" "password1" {
-    provider     = azurerm.prod-subscription
-    name         = "password-1"
-    value        =  random_password.password1.result
-    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+  provider     = azurerm.prod-subscription
+  name         = "password-1"
+  value        = random_password.password1.result
+  key_vault_id = data.azurerm_key_vault.Azueusingterraform.id
 }
 
 resource "azurerm_key_vault_secret" "password2" {
-    provider     = azurerm.prod-subscription
-    name         = "password-2"
-    value        =  random_password.password2.result
-    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+  provider     = azurerm.prod-subscription
+  name         = "password-2"
+  value        = random_password.password2.result
+  key_vault_id = data.azurerm_key_vault.Azueusingterraform.id
 }
 
 resource "azurerm_key_vault_secret" "password3" {
-    provider     = azurerm.prod-subscription
-    name         = "password-3"
-    value        =  random_password.password3.result
-    key_vault_id =  data.azurerm_key_vault.Azueusingterraform.id
+  provider     = azurerm.prod-subscription
+  name         = "password-3"
+  value        = random_password.password3.result
+  key_vault_id = data.azurerm_key_vault.Azueusingterraform.id
 }
 
 
