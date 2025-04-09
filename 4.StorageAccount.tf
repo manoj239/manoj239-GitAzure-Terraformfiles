b3b8d19a45ba382cd6fc2e82225885e59727bf32
MANOJ <manojborra@ebsco.com>
2025-04-09 17:17:26 +0530

    Added Stylish value

--- 4.StorageAccount.tf
+++ 4.StorageAccount.tf
@@ -33,11 +33,8 @@ resource "azurerm_storage_container" "superstar" {
   container_access_type = "private"
 }
 
-
-
-
 resource "azurerm_storage_container" "stylishstar" {
-  name                  = "rowdystar"
+  name                  = "stylishstar"
   storage_account_id    = azurerm_storage_account.devsecopsmanojtfstate.id
   container_access_type = "private"
 }
